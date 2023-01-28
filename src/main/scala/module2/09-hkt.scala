5ef8b45c7d0c51a9abb60ed9e2bbe884477676a5
segi <freeco@mail.ru>
2023-01-28 11:34:13 +0300

    Домашнее задание HTK, Implicits реализован метод tupleF

--- src/main/scala/module2/09-hkt.scala
+++ src/main/scala/module2/09-hkt.scala
@@ -1,6 +1,8 @@
 package module2
 
-object higher_kinded_types{
+import scala.language.implicitConversions
+
+object higher_kinded_types extends App{
 
   def tuple[A, B](a: List[A], b: List[B]): List[(A, B)] =
     a.flatMap{ a => b.map((a, _))}
@@ -12,12 +14,15 @@ object higher_kinded_types{
     a.flatMap{ a => b.map((a, _))}
 
 
-  def tuplef[F[_], A, B](fa: F[A], fb: F[B]): F[(A, B)] = ???
 
 
   trait Bindable[F[_], A] {
     def map[B](f: A => B): F[B]
     def flatMap[B](f: A => F[B]): F[B]
+  }
+
+  abstract class Conv[F[_]] {
+    def conv[A](fa: F[A]): Bindable[F, A]
   }
 
   def tupleBindable[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] = {
@@ -31,6 +36,22 @@ object higher_kinded_types{
     override def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
   }
 
+  def listBindable[A](lst: List[A]): Bindable[List, A] = new Bindable[List, A] {
+    override def map[B](f: A => B): List[B] = lst.map(f)
+
+    override def flatMap[B](f: A => List[B]): List[B] = lst.flatMap(f)
+  }
+
+  implicit val optBindableConv: Conv[Option] = new Conv[Option] {
+    override def conv[A](fa: Option[A]): Bindable[Option, A] = optBindable(fa)
+  }
+
+  implicit val listBindableConv: Conv[List] = new Conv[List] {
+    override def conv[A](fa: List[A]): Bindable[List, A] = listBindable(fa)
+  }
+
+  def tupleF[F[_], A, B](fa: F[A], fb: F[B])(implicit con: Conv[F]): F[(A, B)] =
+    tupleBindable(con.conv(fa), con.conv(fb))
 
   val optA: Option[Int] = Some(1)
   val optB: Option[Int] = Some(2)
@@ -38,10 +59,10 @@ object higher_kinded_types{
   val list1 = List(1, 2, 3)
   val list2 = List(4, 5, 6)
 
-  val r3 = println(tupleBindable(optBindable(optA), optBindable(optB)))
+//  val r3 = println(tupleBindable(optBindable(optA), optBindable(optB)))
  // val r4 = ???
 
-//  val r1 = println(tuplef(optA, optB))
-//  val r2 = println(tuplef(list1, list2))
+  val r1 = println(tupleF(optA, optB))
+  val r2 = println(tupleF(list1, list2))
 
 }
